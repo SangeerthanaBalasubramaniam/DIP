f69f283658f92583ea3af1b9f4b7a7a845874cc1
SangeerthanaBalasubramaniam <87514384+SangeerthanaBalasubramaniam@users.noreply.github.com>
2021-11-09 12:23:09 +0530

    Add files via upload

---
+++ smoothing.py
@@ -0,0 +1,11 @@
+import sys
+import cv2
+import numpy as np
+
+img = cv2.imread(sys.argv[1])
+
+filter_matrix = np.ones((11,11),np.float32)/(11*11)
+dst = cv2.filter2D(img,-1, filter_matrix)
+
+cv2.imwrite("Output.jpg", dst)
+cv2.imwrite("Input.jpg", img)
